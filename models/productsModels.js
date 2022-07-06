59ab9b8485d3492c15cda1cdc9403ccb47af886d
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-07-05 22:54:07 -0300

    Feat Criando crud update, delete, getmodelsById

--- models/productsModels.js
+++ models/productsModels.js
@@ -10,10 +10,48 @@ const getByIdModels = async (id) => {
   const [product] = await db
     .query('SELECT * FROM StoreManager.products WHERE id = ?', [id]);
   return product;
- };
+};
+ 
+const createModels = async (name) => {
+  const [{ id }] = await db.execute(
+    'INSERT INTO StoreManager.products (name) VALUES (?)',
+    [name],
+  );
+  const product = {
+    id,
+    name,
+  };
+  return product;
+};
+
+const updateModels = async (id, name) => {
+  const [affectedRows] = await db.execute(
+    'UPDATE StoreManager.products SET name = ? WHERE id = ?',
+    [name, id],
+  );
+  return affectedRows;
+};
+
+const deleteModels = async (id) => { 
+  const affectedRows = await db.execute(
+    'DELETE FROM StoreManager.products WHERE id = ?',
+    [id],
+  );
+  return affectedRows;
+};
+
+const getModelsById = async (i) => { 
+  const product = await db
+    .query(`SELECT * FROM StoreManager.products WHERE name LIKE "%${i}%"`);
+  return product;
+};
 
 module.exports = {
   getModels,
   getByIdModels,
+  createModels,
+  updateModels,
+  deleteModels,
+  getModelsById,
   
 };
