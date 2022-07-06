1eae65b0f90d0545d4af3caec2b5af6d9f2100e3
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-07-06 11:42:06 -0300

    feat: adicionando tratamento de erros com try cath

--- models/productsModels.js
+++ models/productsModels.js
@@ -1,49 +1,75 @@
 const db = require('./db');
 
-const getModels = async () => { 
-  const [products] = await db
-    .query('SELECT * FROM StoreManager.products');
-  return products;
+const logError = (error) => {
+  console.log(error.message);
 };
 
-const getByIdModels = async (id) => {
-  const [product] = await db
-    .query('SELECT * FROM StoreManager.products WHERE id = ?', [id]);
-  return product;
+const getModels = async () => {
+  try {
+    const [products] = await db
+      .query('SELECT * FROM StoreManager.products');
+    return products;
+  } catch (error) {
+    logError(error);
+  }
+};
+  const getByIdModels = async (id) => {
+  try {
+    const [product] = await db
+      .query('SELECT * FROM StoreManager.products WHERE id = ?', [id]);
+    return product;    
+  } catch (error) {
+    logError(error);
+  }
 };
  
-const createModels = async (name) => {
-  const [{ insertId }] = await db.execute(
-    'INSERT INTO StoreManager.products (name) VALUES (?)',
-    [name],
-  );
-  const product = {
-    id: insertId,
-    name,
-  };
-  return product;
-};
-
-const updateModels = async (id, name) => {
+  const createModels = async (name) => {
+ try {
+      const [{ insertId }] = await db.execute(
+        'INSERT INTO StoreManager.products (name) VALUES (?)',
+        [name],
+      );
+      const product = {
+        id: insertId,
+        name,
+      };
+      return product;
+    } catch (error) {
+      logError(error);
+    }
+ };
+  const updateModels = async (id, name) => {
+ try {
   const [affectedRows] = await db.execute(
     'UPDATE StoreManager.products SET name = ? WHERE id = ?',
     [name, id],
   );
   return affectedRows;
+ } catch (error) {
+    logError(error);
+  }
 };
 
-const deleteModels = async (id) => { 
+  const deleteModels = async (id) => { 
+try {
   const affectedRows = await db.execute(
     'DELETE FROM StoreManager.products WHERE id = ?',
     [id],
   );
   return affectedRows;
+} catch (error) {
+  logError(error);
+}  
 };
 
-const getModelsById = async (i) => { 
+  const getModelsById = async (i) => { 
+try {
   const product = await db
     .query(`SELECT * FROM StoreManager.products WHERE name LIKE "%${i}%"`);
-  return product;
+  return product;  
+} catch (error) {
+ logError(error); 
+}
 };
 
 module.exports = {
