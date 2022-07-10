f5d30f9d9cb1b4b43a2ab8e38b40f804a285121a
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-07-10 15:55:50 -0300

    FIX: Remodulando Midlleware de erro para validar nomes e Id das rotas product e product/ID

--- middlewares/validProduct.js
+++ middlewares/validProduct.js
@@ -1,10 +1,25 @@
-module.exports = (req, res, next) => {
+const { getByIdModels } = require('../models/productsModels');
+
+const validar = (req, res, next) => {
   const { name } = req.body;
-  if (!name) {
-    return next({ status: 400, message: '"name" is required' });
-}  
-  if (name.length < 5) { 
-    return next({ status: 422, message: '"name" length must be at least 5 characters long' });
-  }
+  if (!name) return next({ status: 400, message: '"name" is required' });
+  
+  if (name.length < 5) {
+    return next({ status: 400, message: '"name" must be at least 5 characters' }); 
+  } 
   next();
+};
+
+const validarId = async (req, res, next) => {
+  const { id } = req.params;
+  const [search] = await getByIdModels(id);
+  if (search === undefined) return next({ status: 400, message: 'Product not found' });
+ 
+  next();
+};
+
+module.exports = {
+  validar,
+  validarId,
+
 };
