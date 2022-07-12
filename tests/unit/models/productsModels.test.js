e9eb2ca676d6726c740c733dc8ace4bc5cd60049
jose-cleiton <cleitons835@gmail.com>
2022-07-11 22:31:48 -0300

    Feat : Testanto as funcoes da camada model

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -1,84 +1,78 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const db = require('../../../models/db');
+const nome = 'José Cleiton';
+const nome2 = 'José Cleiton Cerqueira Santos'; 
+const {
+  getModels,
+  getByIdModels,
+  createModels,
+  updateModels,
+  deleteModels,
+  getModelsById,
+} = require('../../../models/productsModels');
 
-const models = require('../../../models/productsModels');
-
-const productsDB = [
-  {
-    id: 1,
-    name: 'Martelo de Thor',
-  },
-  {
-    id: 2,
-    name: 'Traje do Encolhimento',
-  },
-  {
-    id: 3,
-    name: 'Escudo do Capitão América',
-  },
+const testando =[
+  { id: 1, name: 'Martelo de Thor' },
+  { id: 2, name: 'Traje do Encolhimento' },
+  { id: 3, name: 'Escudo do Capitão América' }
 ];
 
-describe('Testa o retorno de funções da camada MODEL relacionadas aos endpoints /products e /products/:id', () => {
+describe('Testando o retorno das funcçoes da camada Model', () => {
   before(async () => {
-    sinon.stub(connection, 'execute').returns(productsDB);
-  });
-
+    sinon.stub(db, 'query').returns(testando);
+  })
   after(async () => {
-    connection.execute.restore();
+    db.query.restore();
+  }
+  )
+
+
+  it('Testando o retorna da função getModels()', async () => {
+    const result = await getModels();
+    
+    expect(result).to.be.an('object');
+    expect(result).to.have.a.property('name');
+    expect(result).to.have.a.property('id');
+    expect(result.name).to.be.a('string');
+    expect(result.id).to.be.a('number');
+    expect(result.id).to.be.equal(1);
+    expect(result.name).to.be.equal('Martelo de Thor');
+    
   });
-
-  it('Deve retornar um array de objetos com todos os produtos', async () => {
-    const result = await models.getProducts();
-    expect(result).to.be.an('array');
-    expect(result).to.have.lengthOf(3);
-    expect(result[0]).to.have.property('id');
-    expect(result[0]).to.have.property('name');
-  })
-
-  it('Deve retornar um objeto com um produto específico', async () => {
-    const result = await models.getProductById(1);
+  it('Testando o retorna da função getByIdModels', async () => {
+    const result = await getByIdModels(1);
     expect(result).to.be.an('object');
-    expect(result).to.have.property('id');
-    expect(result).to.have.property('name');
-  })
+    expect(result).to.have.a.property('name');
+    expect(result).to.have.a.property('id');
+    expect(result.name).to.be.a('string');
+    expect(result.id).to.be.a('number');
+    expect(result.id).to.be.equal(1);
+    expect(result.name).to.be.equal('Martelo de Thor');
+   
+  });
+  it('Testando o retorna da função createModels', async () => {
+    const result = await createModels(nome);
+    expect(result).to.be.an('object');
+    expect(result).to.have.a.property('name');
+    expect(result).to.have.a.property('id');
+    expect(result.name).to.be.a('string');
+    expect(result.id).to.be.a('number');
+    expect(result.name).to.be.equal(nome);
+  });
+  it('Testando o retorna da função updateModels', async () => {
+    const result = await updateModels(1, nome2);
+    expect(result).to.be.an('object');
+  }
+    
+  );
+  it('Testando o retorna da função deleteModels', async () => {
+    const [result] = await deleteModels(1);
+    expect(result).to.be.a('object');
+    expect(result).to.have.a.property('affectedRows');
+    expect(result.affectedRows).to.be.a('number');
+   
 
-  it('Testa a funcao createModels()', async () => {
-    const result = await models.createProduct('Martelo de Thor');
-    expect(result).to.be.an('object');
-    expect(result).to.have.property('id');
-    expect(result).to.have.property('name');
   })
-  
-  it('Testa a funcao updateModels()', async () => {
-    const result = await models.updateProduct(1, 'Martelo de Thor');
-    expect(result).to.be.an('object');
-    expect(result).to.have.property('id');
-    expect(result).to.have.property('name');
-
-  })
-  it('Testa a funcao deleteModels()', async () => {
-    const result = await models.deleteProduct(1);
-    expect(result).to.be.an('object');
-    expect(result).to.have.property('id');
-    expect(result).to.have.property('name');
-  
-  })
-  
-  it('Testa a funcao getModelsById()', async () => {
-    const result = await models.getProductById(1);
-    expect(result).to.be.an('object');
-    expect(result).to.have.property('id');
-    expect(result).to.have.property('name');
-  })
-
-  it('Testa a funcao getModelsById()', async () => { 
-    const result = await models.getProductById(1);
-    expect(result).to.be.an('object');
-    expect(result).to.have.property('id');
-    expect(result).to.have.property('name');
-  })
-
+    
 });
-
-  
